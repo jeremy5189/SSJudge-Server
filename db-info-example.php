cdc62c442035cd3524e220e9c5de6667e12af9de
Jeremy Yen <jeremy5189@gmail.com>
2012-10-03 23:03:54 +0800

    Add db-info-example

---
+++ db-info-example.php
@@ -0,0 +1,15 @@
+<?php
+$db_Address = "127.0.0.1";
+$db_Username = "";
+$db_Password = "";
+$db_Name = "";
+$table_Name= ""; 
+
+$add_code = ""; //add.php POST input code
+$access = ""; // result.php GET access code
+$code_access = ""; // code.php access code 
+$dl_code = ""; // code.php download file access code
+
+$teacher_website = "http://www.google.com";
+
+?>
